74b562710d75cd48ae1cda0ff63849cf05d73ef4
rakatanui <cantripsun@gmail.com>
2017-04-20 19:42:49 +0300

    'StartLayout'

---
+++ layout/index.php
@@ -0,0 +1,38 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Голая верстка</title>
+
+    <script src="/layout/index.js" type="text/javascript"></script>
+    <link href="/layout/style.css" rel="stylesheet">
+    <link href="/layout/responsive.css" rel="stylesheet">
+</head>
+<body>
+<div class="top-block">
+    <div class="wrapper">
+        <header>
+            <div class="top-phone">
+                +375 (29) 269-39-99<br/>
+                Перезвонить вам?
+            </div>
+        </header>
+        <div> <!-- обертка на блоки в шапке -->
+            <div>
+                <!-- заголовок -->
+                Лучшая цена<br/>
+                на розы из Эквадора и Колумбии<br/>
+                от 2,5р за услугу
+            </div>
+            <div><!-- форма обратной связи --></div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
