68b26426bdd4d0814bd4c1255c85311dc989237b
rakatanui <cantripsun@gmail.com>
2017-05-02 15:42:54 +0300

    'Footer'

--- layout/index.php
+++ layout/index.php
@@ -250,16 +250,18 @@
 <footer>
     <div class="wrapper footer-wrapper">
         <div class="left-block">
-            <div class="title_big_w">
+            <div class="title_big title_big_w">
                 Наши контакты
             </div>
-            <div>
+            <div class="foot-italic">
                 Приезжайте! Мы предложим большой выбор цветов и букетов
             </div>
-            <div>Московская</div>
-            <div>пр-т Независимости, 145</div>
-            <div>Звоните! Мы ответим на ваши вопросы</div>
-            <div class="top-phone"></div>
+            <div class="metro">Московская</div>
+            <div class="adress">пр-т Независимости, 145</div>
+            <div class="foot-italic">Звоните! Мы ответим на ваши вопросы</div>
+            <div class="top-phone">
+                <a class="top-phone__top" href="tel:+375292693999">+375 (29) 269-39-99</a>
+            </div>
         </div>
         <div class="form-picking right-block wrapper_narrow">
             <p class="form-picking_title">Поможем подобрать букет к любому событию</p>
@@ -272,9 +274,9 @@
             <input class="button" type="button" value="Подобрать букет">
         </div>
     </div>
-    <div>
-        <div>2017, Акменита</div>
-        <div>Разработка сайта<a class="copiright">Медиа Лайн</a></div>
+    <div class="wrapper bottom-footer">
+        <div>&#169; 2017, Акменита</div>
+        <div>Разработка сайта<a href="http://www.medialine.by" target="_blank" class="copiright"><img src="image/copiright.png" /></a></div>
     </div>
 </footer>
 </body>
